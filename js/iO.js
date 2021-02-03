1fdecb5cb51dfea25cf6e20913bc9ab782c3f574
Denis-Ozolin <ozolindenx@gmail.com>
2021-02-04 00:55:20 +0200

    test intersectionObsrver

---
+++ js/iO.js
@@ -0,0 +1,26 @@
+const galleryImgRef = document.querySelectorAll('.gallery-img');
+
+// function renderImg(images){
+//     images.forEach(image => {
+//         image.src = image.dataset.link;
+//     })
+// }
+// renderImg(galleryImgRef);
+
+const options = {
+    rootMargin: '20px',
+}
+
+const io = new IntersectionObserver((entries, obsrever) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            console.log(entry.target);
+            entry.target.src = entry.target.dataset.link;
+            obsrever.unobserve(entry.target);
+        }
+    })
+}, options)
+
+galleryImgRef.forEach(img => io.observe(img));
+
