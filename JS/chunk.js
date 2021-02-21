74c92c5b7f2c1967c0794661b5267b90e7168a2e
Denis-Ozolin <ozolindenx@gmail.com>
2021-02-21 13:25:54 +0200

    test

--- JS/chunk.js
+++ JS/chunk.js
@@ -4,15 +4,17 @@
 const data = [1, 3, 5, 6, 7, 8, 5, 3, 2, 4, 7, 9, 4];
 
 const chunk = (arr, size) => arr.reduce((acc, item) => {
-        const nextArray = acc[acc.length - 1];
-        if (nextArray.length < size) {
-            nextArray.push(item);
-        } else {
-            acc.push([item]);
-        }
-        return acc;
-    }, [[]]);
+   const nextArray = acc[acc.length - 1];
+   if (nextArray.length < size) {
+      nextArray.push(item);
+   } else {
+      acc.push([item]);
+   }
+   return acc;
+}, [[]]);
 
 console.log(chunk(data, 2));
 console.log(chunk(data, 3));
 console.log(chunk(data, 4));
+
+
